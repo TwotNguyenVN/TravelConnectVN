90439236b077b47f3acb3d13c172ecdbf453837c
zTwotz <nguyenngoctinh011258@gmail.com>
2026-06-07 10:31:18 +0700

    fix(ci): fix github actions database url and unused swagger import

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,6 +2,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
